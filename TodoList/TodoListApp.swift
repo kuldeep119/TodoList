66b66dfb4ac5b7f6078bbf4833ad5aa244ea07cf
Kuldeep <kuldeepsingh41204@gmail.com>
2026-01-07 23:34:21 +0530

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by kuldeep Singh on 07/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
