8f590af6780989fc2f8d343024b32dc9a3f3bebe
Kuldeep <kuldeepsingh41204@gmail.com>
2026-01-21 00:23:53 +0530

    update view

---
+++ TodoList/Views/NoView.swift
@@ -0,0 +1,18 @@
+//
+//  NoView.swift
+//  TodoList
+//
+//  Created by kuldeep Singh on 17/01/26.
+//
+
+import SwiftUI
+
+struct NoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoView()
+}
